3c8f16af8e63e06b0c01eb475e1df70632a75dde
bengigenc <genc.bengi@gmail.com>
2022-08-02 16:01:58 +0300

    Fix linter issues

--- dist/main.js
+++ dist/main.js
@@ -126,7 +126,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _mod
   \*********************************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n// fetch data from the api\n\nconst getData = async () => {\n  await fetch(\"https://api.tvmaze.com/shows\")\n    .then((response) => response.json())\n    .then((json) => displayShow(json).slice(0, 20));\n};\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (getData);\n\n\n//# sourceURL=webpack://movies-database/./src/modules/api-data.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n// fetch data from the api\n\nconst getData = async () => {\n  await fetch('https://api.tvmaze.com/shows')\n    .then((response) => response.json());\n  // .then((json) => displayShow(json).slice(0, 20));\n};\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (getData);\n\n\n//# sourceURL=webpack://movies-database/./src/modules/api-data.js?");
 
 /***/ })
 

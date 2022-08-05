1f1ace4adaf6a2007a82b735f8ac2955986839dd
29td <nonhlanhlamndebele9@gmail.com>
2022-08-05 10:07:43 +0200

    add involvement link

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import "./style.css";
 import getData from "./modules/api-data.js";
 import comments from "./modules/commentPopup";
-import { getLikes, postLikes } from "./modules/likesData";
 
 getData();
 

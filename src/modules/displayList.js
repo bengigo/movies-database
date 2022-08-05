016277f97fa5e91fca18b46201c61fa9a3fb900f
Mike <madetunji1376@gmail.com>
2022-08-05 16:52:18 +0100

    the reservation has been added and is showing the content from the api

--- src/modules/displayList.js
+++ src/modules/displayList.js
@@ -9,8 +9,15 @@ const displayShows = (shows) => {
           </div>
           <p class="genre">${show.genres}</p>
           <button id="${show.id}" class="btn-comments">Comments</button>
-          <button id="${show.id}" class="btn-reservations">Reservations</button>
+          <button id="${show.id}" class="btn-reservations" movie-image="${show.image.medium}" 
+          title="${show.name}" 
+          language="${show.language}"
+          premier="${show.premiered}"
+          ended="${show.ended}"
+          rating="${show.rating.average}"> Book Reservations</button>
           </div>
+
+          
           <dialog></dialog>
         `;
   });

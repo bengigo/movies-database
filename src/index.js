1684135fdeed9bf4afd748754e224b36328531c6
bengigenc <genc.bengi@gmail.com>
2022-08-05 20:54:18 +0300

    Fix linter issues

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import "./style.css";
-import getData from "./modules/api-data.js";
-import comments from "./modules/commentPopup.js";
-import { getLikes } from "./modules/likesData.js";
+import './style.css';
+import getData from './modules/api-data.js';
+import comments from './modules/commentPopup.js';
+import { getLikes } from './modules/likesData.js';
 
 getData();
 

1a35d33955cc2db6969386df35c1fc7b81617be8
29td <nonhlanhlamndebele9@gmail.com>
2022-08-06 18:14:29 +0200

    comments-counter

--- src/modules/commentPopup.js
+++ src/modules/commentPopup.js
@@ -1,20 +1,20 @@
 // import displayComments from "./getComments.js";
 // import fetchComments from './comments.js'
-
+ 
 const comments = () => {
   const getShowData = async (id) => {
     const response = await fetch(`https://api.tvmaze.com/shows/${id}`);
     const showData = await response.json();
     return showData;
   };
-
+ 
   const displayContainer = document.querySelector("#list");
   displayContainer.addEventListener("click", (e) => {
     if (e.target.classList.contains("btn-comments")) {
       const dialog = document.querySelector('dialog');
       dialog.showModal();
       const targetId = e.target.id;
-
+ 
       getShowData(targetId).then((showData) => {
         dialog.innerHTML = `
         <div class="Popup-container">
@@ -27,38 +27,68 @@ const comments = () => {
         <p class="poster-container">${showData.language}</p>
         <p class="poster-container">${showData.summary}</p>
         <br>
-        <h3 class="comments-counter">Comments(0)</h3>
+        <h3 class="comments-counter">Comments(<span>0</span>)</h3>
         <ul class="comment-section">
         </ul>
         <form action="#" class="add-comments" id="comment-form">
         <h3>Add your comment</h3>
         <input type="text" id="name" name="username1" placeholder="Type your name here">
         <textarea id="comments" name="commentMsg1" cols="40" rows="5" placeholder="Write you comment here.."></textarea>
-        <button type="submit" id=${showData} class="submit-comments">Comment</button>
-        </form> -->
+        <button type="submit" id=${showData.id} class="submit-comments">Comment</button>
+        </form>
         `;    
-
+ 
+        fetchComments(targetId)
+     
+        const submit = document.querySelector('.submit-comments');
+ 
+        submit.addEventListener('click', (e)=> {
+          e.preventDefault()
+          const commentList = document.querySelector('.comment-section');
+          const name = document.getElementById('name').value.trim();
+          const addComment = document.getElementById('comments').value.trim();
+          const form = document.querySelector('form');
+          const newComment = new Comment(+e.target.id, name, addComment)
+          const date = Date().split(' ').splice(1, 3).join(' ')
+                  .split(' ')
+                   .reverse();
+          const month = ('JanFebMarAprMayJunJulAugSepOctNovDec'.indexOf(date.slice(2).join('')) / 3 + 1);
+          const commentCount = document.querySelector('.comments-counter span');
+       
+ 
+          if (name && addComment) {
+            (postComment(newComment))
+            commentList.innerHTML += `<li>${date[0].concat(`-0${month}-${date[1]}`)} ${name}: ${addComment}</li>`
+            let count = +commentCount.textContent
+            count+=1
+            commentCount.textContent = count;
+          }      
+          form.reset();  
+        })
+ 
       });
     };
-
-
+ 
+ 
     if (e.target.classList.contains("close")) {
       const dialog = document.querySelector('dialog');
       dialog.close();
     }
-
+ 
     const createComment = (comments) => {
       const commentCount = document.querySelector('.comments-counter');
-      commentCount.innerHTML = `comments(${comments.length})`;
+      commentCount.innerHTML = `Comments (<span>${comments.length}</span>)`;
       const commentList = document.querySelector('.comment-section');
       commentList.innerHTML = '';
       comments.forEach((comment) => {
         commentList.innerHTML += `<li>${comment.creation_date}: ${comment.username}: ${comment.comment}</li>`;
       });
     };
-    
-    const apiCommentURL = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/6Rsc5dWsUkxFOihStX7m/comments';
-    
+ 
+   
+   
+    const apiCommentURL = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/x3jKfe1LZsfM9ZMX6ICC/comments';
+   
     const fetchComments = async (id) => {
       const request = new Request(`${apiCommentURL}?item_id=${id}`);
       const response = await fetch(request);
@@ -68,50 +98,30 @@ const comments = () => {
       const getComment = await response.json();
       createComment(getComment);
     };
-    
-    
-     
-    const displayComment = async (e) => {
-      e.preventDefault();
-      const movieID = e.target;
-    
-      const name = document.getElementById('name').value.trim();
-      const addComment = document.getElementById('comments').value.trim();
-      const commentForm = document.getElementById('comment-form');
-    
-      if (name && addComment) {
-        const result = await fetch(apiCommentURL, {
-          method: 'POST',
-          headers: {
-            'Content-type': 'application/json; charset=UTF-8',
-          },
-          body: JSON.stringify({
-            item_id: `${movieID.id}`,
-            username: `${name}`,
-            comment: `${addComment}`,
-          }),
-        });
-        commentForm.reset();
-        await result.text();
-        fetchComments(movieID.id);
+ 
+    class Comment {
+      constructor(item_id, username, comment) {
+        this.item_id = item_id;
+        this.username = username;
+        this.comment = comment;
       }
-    };
-    fetchComments(id);
-    const submitComment = document.querySelector('.submit-comments');
-    submitComment.addEventListener('click', displayComment);
-    
-    // const commentBtns = document.querySelectorAll('.btn-comments');
-    // commentBtns.forEach((button) => {
-    //   button.addEventListener('click', (e) => {
-    //     comments(e.target.parentNode.id);
-    //     displaySeries.style.filter = 'blur(12px)';
-      // });
-    // });
-    
-    // export default fetchComments();
+    }
+ 
+    const postComment = async (comment) => {
+      const posts = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/x3jKfe1LZsfM9ZMX6ICC/comments', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(comment),
+      });
+      const result = await posts.json();
+      return result;
+    };    
+     
   });
-
+ 
 };
-
-
+ 
+ 
 export default comments;

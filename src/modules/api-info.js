71b21b362dcbee4bde6ba1ea5636dc6261f00ff6
Mike <madetunji1376@gmail.com>
2022-08-06 09:08:03 +0100

    counting the number of reservations booked

--- src/modules/api-info.js
+++ src/modules/api-info.js
@@ -2,21 +2,24 @@
 const endpointLink =
   'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/j2re1UnEtKqKdvrLaCym/reservations/';
 
-const fetchReservations = async (movieId) => fetch(`${endpointLink}?item_id=${movieId}`)
-  .then((res) => res.json())
-  .then((data) => (data.error ?
-    {
+const fetchReservations = async (movieId) =>
+  fetch(`${endpointLink}?item_id=${movieId}`)
+    .then((res) => res.json())
+    .then((data) =>
+      data.error
+        ? {
+            error: true,
+            debug: data,
+            msg: data.error.message,
+          }
+        : {
+            error: false,
+            data,
+          }
+    )
+    .catch(() => ({
       error: true,
-      debug: data,
-      msg: data.error.message,
-    } :
-    {
-      error: false,
-      data,
-    }))
-  .catch(() => ({
-    error: true,
-    msg: 'Something went wrong, please try again later',
-  }));
+      msg: 'Something went wrong, please try again later',
+    }));
 
 export default fetchReservations;
--- src/modules/reservationPopUp.js
+++ src/modules/reservationPopUp.js
@@ -1,6 +1,5 @@
 import fetchReservations from './api-info.js';
 import addReservation from './addReservation.js';
-
 const displayReserve = () => {
   const showTheReservePopUp = ({ ...info }) => {
     const thePopUpInfo = document.querySelector('#show-the-reservation');
@@ -25,11 +24,11 @@ const displayReserve = () => {
             </li>
         </ol>
         <h3 class="reservation-title">
-        reservation (<span class="total-reservations">0</span>)
+        reservation (<span class="numberOf-reservations-booked">0</span>)
       </h3>
-      <ul class="reservations">
+      <span class="reservations">
           reservation data
-      </ul>
+      </span>
       
       <h2>Book Reservation</h2>
       <div class="Reserv-form">
@@ -40,7 +39,7 @@ const displayReserve = () => {
       </div>
         `;
     const hideReservationBtn = thePopUpInfo.querySelector(
-      '.material-symbols-outlined',
+      '.material-symbols-outlined'
     );
     thePopUpInfo.style.display = 'flex';
 
@@ -50,10 +49,15 @@ const displayReserve = () => {
     const data = () => {
       fetchReservations(info.id).then((res) => {
         const reservations = thePopUpInfo.querySelector('.reservations');
+        const reservationsCounter = thePopUpInfo.querySelector(
+          '.numberOf-reservations-booked'
+        );
         if (res.error === true) {
-          reservations.innerHTML = 'Book a Reservation, we are expecting you.';
+          reservationsCounter.innerHTML = '0';
+          reservations.innerHTML = 'Book a reservation, we are expecting you';
         } else {
           reservations.innerHTML = '';
+          reservationsCounter.innerHTML = res.data.length;
           res.data.forEach((reservation) => {
             reservations.innerHTML += `
             <li>From ${reservation.date_start} to ${reservation.date_end} by ${reservation.username}</li>
@@ -75,13 +79,18 @@ const displayReserve = () => {
     });
     fetchReservations(info.id).then((res) => {
       const reservations = thePopUpInfo.querySelector('.reservations');
+      const reservationsCounter = thePopUpInfo.querySelector(
+        '.numberOf-reservations-booked'
+      );
       if (res.error === true) {
-        reservations.innerHTML = 'Book a Reservation, we are expecting you.';
+        reservationsCounter.innerHTML = '0';
+        reservations.innerHTML = 'Book a reservation, we are expecting you';
       } else {
         reservations.innerHTML = '';
+        reservationsCounter.innerHTML = res.data.length;
         res.data.forEach((reservation) => {
           reservations.innerHTML += `
-          <li><strong>From</strong> ${reservation.date_start} <strong>to</strong> ${reservation.date_end} <strong>by</strong> ${reservation.username}</li>
+          <li><strong>From</strong> ${reservation.date_start} <strong>to</strong> ${reservation.date_end}<strong> by</strong> ${reservation.username}</li>
           `;
         });
       }
